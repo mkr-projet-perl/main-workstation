bed13f9100251f953dd4c2e3a1475282e0ceb920
romain huret <romain.boulot62@gmail.com>
2014-12-04 18:51:50 +0100

    FIX: Registre / Ajout d'un listing du registre

--- RegistryKey.pm
+++ RegistryKey.pm
@@ -9,52 +9,64 @@ sub new {
 	my $self = {};
 	bless($self, $classe);
 	
-	$self->{_root}				= $ref_args->{root};
-	$self->{_registryKeyName}	= $ref_args->{registryKeyName};
-	$self->{_registryKeyValues}	= $ref_args->{registryValues};
+	$self->{_name}				= $ref_args->{name};
+	$self->{_fullName}			= $ref_args->{fullName};
+	$self->{_values}			= $ref_args->{registryValues};
+	$self->{_isALeaf}			= $ref_args->{isALeaf};
 	
 	return $self;
 }
 
-sub getName {
-	my $self = shift;
-	return $self->{_registryKeyName};
-}
+=pod
 
-sub getRoot {
-	my $self = shift;
-	return $self->{_root};
-}
+=encoding utf8
 
-sub setType {
-	my ($self, $type) = @_;
-	$self->{type} = $type;
-	return;
-}
+=head1 REGISTRYKEY
 
-sub getInstallLocation {
-	my $self = shift;
-	if(defined $self->{_registryKeyValues}) {
-		return $self->{_registryKeyValues}->{_installLocation};
+Package<RegistryKey> - module de représentation d'une clé de registre
+
+=head1 SYNOPSIS
+
+use::strict;
+
+=head1 DESCRIPTION
+
+L'objet RegistryKey représente l'état d'une clé de registre.
+Il possède :
+
+=head1 ATTRIBUTS
+
+=head2 _name
+Cet attribut correspond au nom de la clé de registre.
+
+=head2 _fullName
+Cet attribut correspond au nom complet de la clé. Cela s'en va de la racine HKEY_*
+jusqu'au nom de la clé.
+
+=head2 _values
+Cet attribut représente toutes les valeurs d'une clé. C'est une table de hachage où
+chaque clé est le nom de la valeur. La valeur associé est aussi est une table de hachage
+contenant le type de la valeur et son contenue.
+	_values => {
+		'.bmp' => {
+			'type' 		=> REG_SZ,
+			'Content'	=> 'PBrush'
+		},
+		'.dib' => {
+			'type'		=> REG_SZ,
+			'Content'	=> 'PBrush'
+		},
+		...
 	}
-	return undef;
-}
 
-sub getUninstallString {
-	my $self = shift;
-	if(defined $self->{_registryKeyValues}) {
-		return $self->{_registryKeyValues}->{_uninstallString};
-	}
-	return undef;
-}
+=head2 _isALeaf
+Cet attribut indique si la clé est une feuille ou non. C'est-à-dire si elle ne possèdent aucune sous clés.
 
-sub getDisplayIcon {
-	my $self = shift;
-	if(defined $self->{_registryKeyValues}) {
-		return $self->{_registryKeyValues}->{_displayIcon};
-	}
-	return undef;
-}
+=head1 ATUTEUR
+
+romain huret
+
+=cut
 
 1;
 __END__
--- test.pl
+++ test.pl
@@ -3,23 +3,22 @@ use strict;
 use Win32API::Registry 0.21 qw( :ALL );
 use Data::Dumper;
 
+use constant KEY_WOW64_64KEY => 0x0100;
+use constant KEY_WOW64_32KEY => 0x0200;
+
 sub _openKey {
-	my ($root, $key,$key_way) = @_;
+	my ($root, $key, $key_rights) = @_;
 	my $registry_key;
-	if (RegOpenKeyEx($root, $key, 0, $key_way, $registry_key)) {
-		return $registry_key;
-	} else {
-		die $key."\n".regLastError()."\n";
-	}
+	RegOpenKeyEx($root, $key, 0, $key_rights, $registry_key)
+		or die "RegOpenKeyEx impossible d'ouvrir $root\\$key\n".regLastError()."\n";
+	return $registry_key;
 }
 
 sub _closeKey {
 	 my $key = shift;
-	 if(RegCloseKey($key)) {
-	 	return 1;
-	 } else {
-	 	die $key."\n".regLastError()."\n";
-	 }
+	 RegCloseKey($key)
+	 	or die "RegCloseKey $key\n".regLastError()."\n";
+ 	return 1;
 }
 
 my $_rootRegistryKey = {
@@ -42,7 +41,7 @@ sub _transformRegistryString {
 				$root = $_rootRegistryKey->{$root};
 				if(defined $3) {
 					$key = $3;
-					$key =~ s/\//\\/g;
+					$key =~ s/\//\\\\/g;
 				} else {
 					$key = "";
 				}
@@ -56,37 +55,142 @@ sub _transformRegistryString {
 	return ($root, $key);
 }
 
-use constant KEY_READ64 => 0x0200;
+sub _subKeyCounter {
+	my $opened_key = shift;
+	my $nbSubKeys;
+	RegQueryInfoKey($opened_key, [], [], [], $nbSubKeys, [], [], [], [], [], [], []) 
+		or die "RegQueryInfoKey impossible de compter le nombre sous-clefs".regLastError()."\n";
+	return $nbSubKeys;
+}
+
+sub _valueCounter {
+	my $opened_key = shift;
+	my $nbValues;
+	RegQueryInfoKey($opened_key, [], [], [], [], [], [], $nbValues, [], [], [], []) 
+		or die "RegQueryInfoKey impossible de compter le nombre de valeurs".regLastError()."\n";
+	return $nbValues;
+}
+
+sub _enumSubKeyName {
+	my ($opened_key, $index) = @_;
+	my $subKeyName;
+	RegEnumKeyEx($opened_key, $_, $subKeyName, [], [], [], [], []) 
+		or die "RegEnumKeyEx impossible d'itérer sur les sous-clefs".regLastError()."\n";
+	return $subKeyName;
+}
+
+sub _enumValue {
+	my ($opened_key, $index) = @_;
+	my ($name, $type, $data);
+	RegEnumValue($opened_key, $index, $name, [], [], $type, $data, [])
+		or die "RegEnumValue impossible d'itérer sur les valeurs".regLastError()."\n";
+	return ($name, $type, $data);
+}
+
+sub _getRegistryKeyValue {
+	my ($opened_key, $value) = @_;
+	my ($type, $data);
+	RegQueryValueEx($opened_key, $value, [], $type, $data, []);
+	return ($type, $data);
+}
+
+sub _getAllRegistryKeyValues {
+	my $opened_key = shift;
+	my @tab;
+	my $nbValues = _valueCounter($opened_key);
+	foreach (0..$nbValues-1) {
+		my ($name, $type, $data) = _enumValue($opened_key, $_);
+		push @tab, {$name => {'type' => $type, 'data' => $data}};
+	}
+	return \@tab;
+}
 
 sub _scanRegistry {
 	my ($root, $key, $res) = @_;
-	my ($opened_key, $nbSubKeys, $subKeyName);
-	$opened_key = _openKey($root, $key, KEY_READ|KEY_READ64);
-	RegQueryInfoKey($opened_key, [], [], [], $nbSubKeys, [], [], [], [], [], [], []) or die regLastError()."\n";
+	my ($opened_key, $nbSubKeys);
+	$opened_key = _openKey($root, $key, KEY_READ|0x0200);
+	$nbSubKeys = _subKeyCounter($opened_key);
 	if($nbSubKeys) {
 		foreach (0..$nbSubKeys-1) {
-			RegEnumKeyEx($opened_key, $_, $subKeyName, [], [], [], [], []) or die regLastError()."\n";
+			my $subKeyName = _enumSubKeyName($opened_key, $_);
 			_scanRegistry($opened_key, $subKeyName, $res);
 		}
 	} else {
-		push @$res, $key;
+		my $values = _getAllRegistryKeyValues($opened_key);
+		$res->{$key} = $values;
 	}
 	_closeKey($opened_key);
+}
+
+sub _compareValue {
+	my ($keyValuesA, $keyValuesB) = @_;
+	my $isSame = 1;
+	$isSame = 0 if($keyValuesA->{'name'} ne $keyValuesB->{'name'});
+	$isSame = 0 if($keyValuesA->{'type'} != $keyValuesB->{'type'});
+	$isSame = 0 if($keyValuesA->{'data'} ne $keyValuesB->{'data'});
+	return $isSame;
+}
+
+sub _compareRegistryKey {
+	my ($valuesA, $valuesB) = @_;
+	my $isSame = 0;
+	foreach (@$valuesA) {
+		
+	}
+	return $isSame;
 }
 
 sub scanRegistry {
 	my ($path) = @_;
 	my ($root, $key) = _transformRegistryString($path);
-	my @res;
+	my %res;
+	print "$root\t$key\n";
 	if(defined $root) {
-		_scanRegistry($root, $key, \@res);
+		_scanRegistry($root, $key, \%res);
 	} else {
 		print "$path n'est pas une clé valide\n";
 	}
-	return \@res;
+	return \%res;
 }
 
-print Dumper(scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/"));
+sub diffRegistry {
+	my ($oldRegistry, $newRegistry) = @_;
+	my (@deleted, @updatings, @news); 
+	my %res;
+	foreach (keys(%$oldRegistry)) {
+		if(!exists($newRegistry->{$_})) {
+			push @deleted, $oldRegistry->{$_};
+		} else {
+			#On regarde si elle a été modifiée (valeur, type, contenue)
+			my $values = $oldRegistry->{$_};
+			foreach (@$values) {
+				if(!_compareRegistryKey($oldRegistry->{$_}, $newRegistry->{$_})) {
+					push @updatings, $oldRegistry->{$_};
+				}
+			}
+		}
+	}
+	foreach (keys(%$newRegistry)) {
+		if(!exists($oldRegistry->{$_})) {
+			push @news, $newRegistry->{$_};
+		}
+	}
+	$res{'delete'} = \@deleted;
+	$res{'update'} = \@updatings;
+	$res{'news'} = \@news;
+	return \%res;
+}
+my $res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Applets");
+my $res2 = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication");
+print Dumper($res2);
+#my $hash = diffRegistry($res, $res2);
+#print Dumper($hash);
+
+#$res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/");
+#print scalar(@$res)."\n";
+
+#$res = scanRegistry("LMachine/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall/");
+#print scalar(@$res)."\n";
 
 #print Dumper(_transformRegistryString(""));
 #print Dumper(_transformRegistryString("LMachine"));
