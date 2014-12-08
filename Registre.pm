718d0f641cc8ed136dc8bb3042ac270281a71042
romain huret <romain.boulot62@gmail.com>
2014-12-08 23:18:17 +0100

    FIX: Registre / Module de registre fonctonnelle et mise en place des tests

--- Registre.pm
+++ Registre.pm
@@ -1,49 +1,29 @@
 #!C:\Strawberry\perl\bin\perl -w
 package Registre;
+require Exporter;
 use strict;
 use Win32API::Registry 0.21 qw( :ALL );
-use RegistryKey;
+use Data::Dumper;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(&installLocation &scanRegistry &diffRegistry);
 
-#Compte le nom de clé registre ouverte.
-my $registryKeyCounter = 0;
+use constant KEY_READ_ALL => KEY_READ|0x0200;
 
-sub new {
-	my ($classe, $ref_args) = @_;
-	$classe = ref($classe) || $classe;
-	my $self = {};
-	bless($self, $classe);
-	
-	#Indique si il y a une connexion sur une machine distante
-	$self->{_isConnectedToRemote} = 0;
-	
-	return $self;
+sub _openKey {
+	my ($root, $key, $key_rights) = @_;
+	my $registry_key;
+	RegOpenKeyEx($root, $key, 0, $key_rights, $registry_key)
+		or die "RegOpenKeyEx impossible d'ouvrir $root\\$key\n".regLastError()."\n";
+	return $registry_key;
 }
 
-#################################
-#Méthode privée au package
-#################################
-
-sub _setRegistryKey {
-	my ($self, $key) = @_;
-	if (defined $key) {
-		$self->{_crtRegistryKey} = $key;
-	}
-	return;
+sub _closeKey {
+	 my $key = shift;
+	 RegCloseKey($key)
+	 	or die "RegCloseKey $key\n".regLastError()."\n";
+ 	return 1;
 }
 
-sub _getRegistryKey {
-	my $self = shift;
-	return $self->{_crtRegistryKey};
-}
-
-sub _error {
-	my $key = shift;
-	die $key."\n".regLastError()."\n";
-}
-
-
-#Correspondance de la tête de la clé
-#Ceci correspond au HKEY_*
 my $_rootRegistryKey = {
 	"LMachine"	=> HKEY_LOCAL_MACHINE,
 	"Classes"	=> HKEY_CLASSES_ROOT,
@@ -54,83 +34,258 @@ my $_rootRegistryKey = {
 	"DynDat"	=> HKEY_DYN_DATA
 };
 
-sub _headCorrectRegistryKey {
-	my $key = shift;
-	
-}
+my $_valueType = {
+	1	=> 'REG_SZ',
+	2	=> 'REG_EXPAND_SZ',
+	3	=> 'REG_BINARY',
+	4	=> 'REG_DWORD',
+	7	=> 'REG_MULTI_SZ',
+	9	=> 'REG_FULL_RESOURCE_DESCRIPTOR'
+};
 
-use constant KEY_READ32 => 0x0100;
-use constant KEY_READ64 => 0x0200;
-
-sub _openKey {
-	my ($self,$key,$key_way) = @_;
-	my $registry_key;
-	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, $key, 0, $key_way, $registry_key)) {
-		return $registry_key;
-	} else {
-		$self->_error('Lecture impossible de la clé'.$key);
-	}
-}
-
-sub _closeKey {
-	 my ($self, $key) = @_;
-	 if(RegCloseKey($key)) {
-	 	return 1;
-	 } else {
-	 	$self->_error("Fermeture impossible de la clé ".$key);
-	 }
-}
-
-sub _getValue {
-	my ($self, $registry_key, $value) = @_;
-	my ($type, $data);
-	if(RegQueryValueEx( $registry_key, $value, [], $type, $data, [] )) { 
-		return ($type, $data);
-	} else {
-		$self->_error("Récupération des valeurs impossible pour la clé ".$registry_key."\n");
-	}
-	return undef;
-}
-
-#####################################
-# Méthodes publiques
-#####################################
-
-sub scanRegistry {
-	my ($self, $refPathList, $savedRegistry) = @_;
-	foreach my $crt_path (@$refPathList) {
-		if((my $crt_key = $self->_open($crt_path, KEY_READ|KEY_READ64))) {
-			if(ref($crt_key) eq "SCALAR") {
-				
-			} elsif(ref($crt_key) eq "HASH") {
-				
+sub _transformRegistryString {
+	my $str = shift;
+	my ($root, $key);
+	if($str =~ m/^(\w+)(\/(.+)$)?/) {
+		if(defined $1) {
+			$root = $1;
+			if(exists $_rootRegistryKey->{$root}) {
+				$root = $_rootRegistryKey->{$root};
+				if(defined $3) {
+					$key = $3;
+					$key =~ s/\//\\\\/g;
+				} else {
+					$key = "";
+				}
+			} else {
+				$root = undef;
 			}
-			$self->_close($crt_key);
 		} else {
-			$self->_error();
+			print "$str n'est pas une clé\n";
 		}
 	}
+	return ($root, $key);
 }
 
-sub readRegistryKey {
-	my ($self, $key) = @_;
+sub _transformRegistryValue {
+	my ($type,$value) = @_;
+	my $newValue = $value;
+	if($type == 3) {
+		$newValue = unpack("B*", $value);
+	}elsif($type == 4) {
+		$newValue = unpack("H*", $value);
+	}elsif($type == 9) {
+		$newValue = unpack("H*", $value);
+	}
+	return $newValue;
 }
 
-sub writeRegistry {
-	my ($self, $key) = @_;
+sub _subKeyCounter {
+	my $opened_key = shift;
+	my $nbSubKeys;
+	RegQueryInfoKey($opened_key, [], [], [], $nbSubKeys, [], [], [], [], [], [], []) 
+		or die "RegQueryInfoKey impossible de compter le nombre sous-clefs".regLastError()."\n";
+	return $nbSubKeys;
 }
 
-sub deleteRegistry {
-	my ($self, $key) = @_;
+sub _valueCounter {
+	my $opened_key = shift;
+	my $nbValues;
+	RegQueryInfoKey($opened_key, [], [], [], [], [], [], $nbValues, [], [], [], []) 
+		or die "RegQueryInfoKey impossible de compter le nombre de valeurs".regLastError()."\n";
+	return $nbValues;
 }
 
-sub deleteRegistryKeyValues {
-	my ($self, $key, $values) = @_;
+sub _enumSubKeyName {
+	my ($opened_key, $index) = @_;
+	my $subKeyName;
+	RegEnumKeyEx($opened_key, $_, $subKeyName, [], [], [], [], []) 
+		or die "RegEnumKeyEx impossible d'itérer sur les sous-clefs".regLastError()."\n";
+	return $subKeyName;
 }
 
-sub updateRegistryKey {
-	my ($self, $key, $values) = @_;
+sub _enumValue {
+	my ($opened_key, $index) = @_;
+	my ($name, $type, $data);
+	RegEnumValue($opened_key, $index, $name, [], [], $type, $data, [])
+		or die "RegEnumValue impossible d'itérer sur les valeurs".regLastError()."\n";
+	return ($name, $type, $data);
 }
 
-1;
-__END__
+sub _getRegistryKeyValue {
+	my ($opened_key, $value) = @_;
+	my ($type, $data);
+	RegQueryValueEx($opened_key, $value, [], $type, $data, []);
+	return ($type, $data);
+}
+
+sub _getAllRegistryKeyValues {
+	my $opened_key = shift;
+	my %hash;
+	my $nbValues = _valueCounter($opened_key);
+	foreach (0..$nbValues-1) {
+		my %values;
+		my ($name, $type, $data) = _enumValue($opened_key, $_);
+		$values{'type'} = $_valueType->{$type};
+		$values{'data'} = _transformRegistryValue($type, $data);
+		$hash{$name} = \%values;
+	}
+	return \%hash;
+}
+
+sub _scanRegistry {
+	my ($root, $key, $res, $fullPath) = @_;
+	my ($opened_key, $nbSubKeys);
+	$opened_key = _openKey($root, $key, KEY_READ_ALL);
+	$nbSubKeys = _subKeyCounter($opened_key);
+	if($nbSubKeys) {
+		foreach (0..$nbSubKeys-1) {
+			my $subKeyName = _enumSubKeyName($opened_key, $_);
+			_scanRegistry($opened_key, $subKeyName, $res, $fullPath."/".$subKeyName);
+		}
+	}
+	my $values = _getAllRegistryKeyValues($opened_key);
+	$fullPath =~ s/\\\\/\//g;
+	$res->{$fullPath} = $values;
+	_closeKey($opened_key);
+}
+
+sub _compareRegistryKey {
+	my ($valuesA, $valuesB) = @_;
+	my $isSame = 1;
+	foreach (keys(%$valuesA)) {
+		if(exists $valuesB->{$_}) {
+			if(!$valuesA->{$_}{'type'} eq $valuesB->{$_}{'type'}) {
+				$isSame = 0;
+			}
+			if(!$valuesA->{$_}{'data'} == $valuesB->{$_}{'data'} ||
+				!$valuesA->{$_}{'data'} eq $valuesB->{$_}{'data'}) {
+				$isSame = 0;		
+			}
+		} else {
+			$isSame = 0;
+		} 
+	}
+	$isSame = 0 if(scalar(grep { !exists $valuesA->{$_} } keys(%$valuesB)));
+	return $isSame;
+}
+
+sub _installInformation {
+	my ($registryPath, $appName, $values) = @_;
+	my %installLocation;
+	$appName =~ tr/A-Z/a-z/;
+	my ($root, $key) = _transformRegistryString($registryPath);
+	my $opened_key = _openKey($root, $key, KEY_READ_ALL);
+	my $nbSubKeys = _subKeyCounter($opened_key);
+	foreach (0..$nbSubKeys-1) {
+		my $subKeyName = _enumSubKeyName($opened_key, $_);
+		my $subOpenedKey = _openKey($opened_key, $subKeyName, KEY_READ_ALL);
+		my $name = _getRegistryKeyValue($subOpenedKey, "DisplayName");
+		$name =~ tr/A-Z/a-z/;
+		if($appName eq $name) {
+			if(defined $values) {
+				foreach (@$values) {
+					my ($type, $data) = _getRegistryKeyValue($subOpenedKey, $_);
+					if(defined $data && $data ne '') {
+						$installLocation{$_} = $data;
+					}
+				}
+			} else {
+				my ($type, $data) = _getRegistryKeyValue($subOpenedKey, "InstallLocation");
+				$installLocation{'InstallLocation'} = $data if(defined $data);
+			}
+		}
+		_closeKey($subOpenedKey);
+	}
+	_closeKey($opened_key);
+	return \%installLocation;
+}
+
+sub installLocation {
+	my $appName = shift;
+	my $registryKeyPath32Bits = 'LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall';
+	my $registryKeyPath64Bits = 'LMachine/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall';
+	my @values = qw(InstallLocation UninstallString DisplayIcon);
+	my $installLocation = _installInformation($registryKeyPath32Bits, $appName, \@values);
+	if(!defined $installLocation || $installLocation eq '') {
+		$installLocation = _installInformation($registryKeyPath64Bits, $appName, \@values);
+	}
+	return $installLocation;
+}
+
+sub scanRegistry {
+	my ($path) = @_;
+	my ($root, $key) = _transformRegistryString($path);
+	my %res;
+	print "$root\t$key\n";
+	if(defined $root) {
+		_scanRegistry($root, $key, \%res, $key);
+	} else {
+		print "$path n'est pas une clé valide\n";
+	}
+	return \%res;
+}
+
+sub diffRegistry {
+	my ($oldRegistry, $newRegistry) = @_;
+	my (%deleted, %updatings, %news); 
+	my %res;
+	foreach (keys(%$oldRegistry)) {
+		if(!exists($newRegistry->{$_})) {
+			$deleted{$_} = $oldRegistry->{$_};
+		} else {
+			#On regarde si elle a été modifiée (valeur, type, contenue)
+			my $values = $oldRegistry->{$_};
+			foreach (keys(%$values)) {
+				if(!_compareRegistryKey($oldRegistry->{$_}, $newRegistry->{$_})) {
+					$updatings{$_} = $oldRegistry->{$_};
+				}
+			}
+		}
+	}
+	foreach (keys(%$newRegistry)) {
+		if(!exists($oldRegistry->{$_})) {
+			$news{$_} = $newRegistry->{$_};
+		}
+	}
+	$res{'delete'} = \%deleted;
+	$res{'update'} = \%updatings;
+	$res{'news'} = \%news;
+	return \%res;
+}
+
+sub _createKey {
+	my ($keyPath, $values) = @_;	
+}
+
+sub _replaceKey {
+	my ($keyPath, $values) = @_;
+}
+
+sub _deleteKey {
+	my ($keyPath) = @_;
+}
+
+sub _deleteValues {
+	my ($keyPath, $values) = @_;
+}
+
+#my $install = installLocation('rogue legacy');
+#print Dumper($install);
+
+#my $res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Applets");
+#my $res2 = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication");
+
+#my $hash = diffRegistry($res, $res2);
+#print Dumper($hash);
+
+#$res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/");
+#print scalar(@$res)."\n";
+
+#$res = scanRegistry("LMachine/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall/");
+#print scalar(@$res)."\n";
+
+#print Dumper(_transformRegistryString(""));
+#print Dumper(_transformRegistryString("LMachine"));
+#print Dumper(_transformRegistryString("LMachine/SOFTWARE"));
+#print Dumper(_transformRegistryString("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall"));
--- test.pl
+++ test.pl
@@ -1,271 +1,41 @@
 #!C:\Strawberry\perl\bin\perl -w
 use strict;
-use Win32API::Registry 0.21 qw( :ALL );
 use Data::Dumper;
+use Registre;
+use FileTools;
 
-use constant KEY_READ_ALL => KEY_READ|0x0200;
-
-sub _openKey {
-	my ($root, $key, $key_rights) = @_;
-	my $registry_key;
-	RegOpenKeyEx($root, $key, 0, $key_rights, $registry_key)
-		or die "RegOpenKeyEx impossible d'ouvrir $root\\$key\n".regLastError()."\n";
-	return $registry_key;
-}
-
-sub _closeKey {
-	 my $key = shift;
-	 RegCloseKey($key)
-	 	or die "RegCloseKey $key\n".regLastError()."\n";
- 	return 1;
-}
-
-my $_rootRegistryKey = {
-	"LMachine"	=> HKEY_LOCAL_MACHINE,
-	"Classes"	=> HKEY_CLASSES_ROOT,
-	"CUser"		=> HKEY_CURRENT_USER,
-	"Users"		=> HKEY_USERS,
-	"CConfig"	=> HKEY_CURRENT_CONFIG,
-	"PerfData"	=> HKEY_PERFORMANCE_DATA,
-	"DynDat"	=> HKEY_DYN_DATA
-};
-
-my $_valueType = {
-	1	=> 'REG_SZ',
-	2	=> 'REG_EXPAND_SZ',
-	3	=> 'REG_BINARY',
-	4	=> 'REG_DWORD',
-	7	=> 'REG_MULTI_SZ',
-	9	=> 'REG_FULL_RESOURCE_DESCRIPTOR'
-};
-
-sub _transformRegistryString {
-	my $str = shift;
-	my ($root, $key);
-	if($str =~ m/^(\w+)(\/(.+)$)?/) {
-		if(defined $1) {
-			$root = $1;
-			if(exists $_rootRegistryKey->{$root}) {
-				$root = $_rootRegistryKey->{$root};
-				if(defined $3) {
-					$key = $3;
-					$key =~ s/\//\\\\/g;
-				} else {
-					$key = "";
-				}
-			} else {
-				$root = undef;
-			}
-		} else {
-			print "$str n'est pas une clé\n";
-		}
+=head1
+my $path = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Saints Row IV";
+my %res;
+FileTools::giveFilesInDirectory($path, \%res);
+foreach my $p (keys(%res)) {
+	print "$p\n";
+	my $subRes = $res{$p};
+	foreach (keys(%$subRes)) {
+		print "\t$_\n\t$subRes->{$_}\n";
 	}
-	return ($root, $key);
+}
+print scalar(keys(%res))."\n";
+=cut
+
+my $exe = "C:\\Users\\romain\\Downloads\\npp.6.6.9.Installer.exe";
+my $save = Registre::scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/");
+
+my $pid = open FILS, "-|";
+if(!defined $pid) {
+	die "Impossible de dupliquer le processus $!\n";
+} elsif($pid) {
+	#Ici le père
+	waitpid($pid, 0);
+	close FILS;
+} else {
+	#Ici le fils de ****
+	exec "$exe";
 }
 
-sub _transformRegistryValue {
-	my ($type,$value) = @_;
-	my $newValue = $value;
-	if($type == 3) {
-		$newValue = unpack("B*", $value);
-	}elsif($type == 4) {
-		$newValue = unpack("H*", $value);
-	}elsif($type == 9) {
-		$newValue = unpack("H*", $value);
-	}
-	return $newValue;
-}
+my $save2 = Registre::scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/");
+my $diff = Registre::diffRegistry($save, $save2);
 
-sub _subKeyCounter {
-	my $opened_key = shift;
-	my $nbSubKeys;
-	RegQueryInfoKey($opened_key, [], [], [], $nbSubKeys, [], [], [], [], [], [], []) 
-		or die "RegQueryInfoKey impossible de compter le nombre sous-clefs".regLastError()."\n";
-	return $nbSubKeys;
-}
-
-sub _valueCounter {
-	my $opened_key = shift;
-	my $nbValues;
-	RegQueryInfoKey($opened_key, [], [], [], [], [], [], $nbValues, [], [], [], []) 
-		or die "RegQueryInfoKey impossible de compter le nombre de valeurs".regLastError()."\n";
-	return $nbValues;
-}
-
-sub _enumSubKeyName {
-	my ($opened_key, $index) = @_;
-	my $subKeyName;
-	RegEnumKeyEx($opened_key, $_, $subKeyName, [], [], [], [], []) 
-		or die "RegEnumKeyEx impossible d'itérer sur les sous-clefs".regLastError()."\n";
-	return $subKeyName;
-}
-
-sub _enumValue {
-	my ($opened_key, $index) = @_;
-	my ($name, $type, $data);
-	RegEnumValue($opened_key, $index, $name, [], [], $type, $data, [])
-		or die "RegEnumValue impossible d'itérer sur les valeurs".regLastError()."\n";
-	return ($name, $type, $data);
-}
-
-sub _getRegistryKeyValue {
-	my ($opened_key, $value) = @_;
-	my ($type, $data);
-	RegQueryValueEx($opened_key, $value, [], $type, $data, []);
-	return ($type, $data);
-}
-
-sub _getAllRegistryKeyValues {
-	my $opened_key = shift;
-	my %hash;
-	my $nbValues = _valueCounter($opened_key);
-	foreach (0..$nbValues-1) {
-		my %values;
-		my ($name, $type, $data) = _enumValue($opened_key, $_);
-		$values{'type'} = $_valueType->{$type};
-		$values{'data'} = _transformRegistryValue($type, $data);
-		$hash{$name} = \%values;
-	}
-	return \%hash;
-}
-
-sub _scanRegistry {
-	my ($root, $key, $res, $fullPath) = @_;
-	my ($opened_key, $nbSubKeys);
-	$opened_key = _openKey($root, $key, KEY_READ_ALL);
-	$nbSubKeys = _subKeyCounter($opened_key);
-	if($nbSubKeys) {
-		foreach (0..$nbSubKeys-1) {
-			my $subKeyName = _enumSubKeyName($opened_key, $_);
-			_scanRegistry($opened_key, $subKeyName, $res, $fullPath."/".$subKeyName);
-		}
-	}
-	my $values = _getAllRegistryKeyValues($opened_key);
-	$fullPath =~ s/\\\\/\//g;
-	$res->{$fullPath} = $values;
-	_closeKey($opened_key);
-}
-
-sub _compareRegistryKey {
-	my ($valuesA, $valuesB) = @_;
-	my $isSame = 1;
-	foreach (keys(%$valuesA)) {
-		if(exists $valuesB->{$_}) {
-			if(!$valuesA->{$_}{'type'} eq $valuesB->{$_}{'type'}) {
-				$isSame = 0;
-			}
-			if(!$valuesA->{$_}{'data'} == $valuesB->{$_}{'data'} ||
-				!$valuesA->{$_}{'data'} eq $valuesB->{$_}{'data'}) {
-				$isSame = 0;		
-			}
-		} else {
-			$isSame = 0;
-		} 
-	}
-	$isSame = 0 if(scalar(grep { !exists $valuesA->{$_} } keys(%$valuesB)));
-	return $isSame;
-}
-
-sub _installInformation {
-	my ($registryPath, $appName, $values) = @_;
-	my %installLocation;
-	$appName =~ tr/A-Z/a-z/;
-	my ($root, $key) = _transformRegistryString($registryPath);
-	my $opened_key = _openKey($root, $key, KEY_READ_ALL);
-	my $nbSubKeys = _subKeyCounter($opened_key);
-	foreach (0..$nbSubKeys-1) {
-		my $subKeyName = _enumSubKeyName($opened_key, $_);
-		my $subOpenedKey = _openKey($opened_key, $subKeyName, KEY_READ_ALL);
-		my $name = _getRegistryKeyValue($subOpenedKey, "DisplayName");
-		$name =~ tr/A-Z/a-z/;
-		if($appName eq $name) {
-			if(defined $values) {
-				foreach (@$values) {
-					my ($type, $data) = _getRegistryKeyValue($subOpenedKey, $_);
-					if(defined $data && $data ne '') {
-						$installLocation{$_} = $data;
-					}
-				}
-			} else {
-				my ($type, $data) = _getRegistryKeyValue($subOpenedKey, "InstallLocation");
-				$installLocation{'InstallLocation'} = $data if(defined $data);
-			}
-		}
-		_closeKey($subOpenedKey);
-	}
-	_closeKey($opened_key);
-	return \%installLocation;
-}
-
-sub installLocation {
-	my $appName = shift;
-	my $registryKeyPath32Bits = 'LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall';
-	my $registryKeyPath64Bits = 'LMachine/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall';
-	my @values = qw(InstallLocation UninstallString DisplayIcon);
-	my $installLocation = _installInformation($registryKeyPath32Bits, $appName, \@values);
-	if(!defined $installLocation || $installLocation eq '') {
-		$installLocation = _installInformation($registryKeyPath64Bits, $appName, \@values);
-	}
-	return $installLocation;
-}
-
-sub scanRegistry {
-	my ($path) = @_;
-	my ($root, $key) = _transformRegistryString($path);
-	my %res;
-	print "$root\t$key\n";
-	if(defined $root) {
-		_scanRegistry($root, $key, \%res, $key);
-	} else {
-		print "$path n'est pas une clé valide\n";
-	}
-	return \%res;
-}
-
-sub diffRegistry {
-	my ($oldRegistry, $newRegistry) = @_;
-	my (%deleted, %updatings, %news); 
-	my %res;
-	foreach (keys(%$oldRegistry)) {
-		if(!exists($newRegistry->{$_})) {
-			$deleted{$_} = $oldRegistry->{$_};
-		} else {
-			#On regarde si elle a été modifiée (valeur, type, contenue)
-			my $values = $oldRegistry->{$_};
-			foreach (keys(%$values)) {
-				if(!_compareRegistryKey($oldRegistry->{$_}, $newRegistry->{$_})) {
-					$updatings{$_} = $oldRegistry->{$_};
-				}
-			}
-		}
-	}
-	foreach (keys(%$newRegistry)) {
-		if(!exists($oldRegistry->{$_})) {
-			$news{$_} = $newRegistry->{$_};
-		}
-	}
-	$res{'delete'} = \%deleted;
-	$res{'update'} = \%updatings;
-	$res{'news'} = \%news;
-	return \%res;
-}
-
-my $install = installLocation('rogue legacy');
-print Dumper($install);
-
-#my $res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Applets");
-#my $res2 = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication");
-
-#my $hash = diffRegistry($res, $res2);
-#print Dumper($hash);
-
-#$res = scanRegistry("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/");
-#print scalar(@$res)."\n";
-
-#$res = scanRegistry("LMachine/SOFTWARE/Wow6432Node/Microsoft/Windows/CurrentVersion/Uninstall/");
-#print scalar(@$res)."\n";
-
-#print Dumper(_transformRegistryString(""));
-#print Dumper(_transformRegistryString("LMachine"));
-#print Dumper(_transformRegistryString("LMachine/SOFTWARE"));
-#print Dumper(_transformRegistryString("LMachine/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall"));
+print "Delete\n".Dumper($diff->{'delete'});
+print "Update\n".Dumper($diff->{'updatings'});
+print "New\n".Dumper($diff->{'news'});
